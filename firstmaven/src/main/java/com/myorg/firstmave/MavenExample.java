d77a84c86ede16bd5f9a361151750f2cf4e49e4a
maheshgitrepo <maheshgitrepo@gmail.com>
2014-05-18 14:33:39 +0100

    first commit

---
+++ firstmaven/src/main/java/com/myorg/firstmave/MavenExample.java
@@ -0,0 +1,11 @@
+package com.myorg.firstmave;
+
+/**
+ * Created by Munni on 18/05/14.
+ */
+public class MavenExample {
+
+    public static void main(String[] args) {
+        System.out.println("My first java");
+    }
+}
